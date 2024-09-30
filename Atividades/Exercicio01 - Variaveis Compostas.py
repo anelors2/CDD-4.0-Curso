6669f2630ed3fd3a4babc643ff5ca1ae1b78bb41
Bruno Souza <brunoaleixosh@gmail.com>
2024-09-30 16:14:33 -0300

    Create Exercicio01 - Variaveis Compostas.py

---
+++ Atividades/Exercicio01 - Variaveis Compostas.py
@@ -0,0 +1,9 @@
+nomes = ['','','','','']
+tamanho = len(nomes)
+for a in range(tamanho):
+    nomes[a]=input('Digite um aluno: ')
+print(nomes)
+
+for i in range(tamanho):
+    print(nomes[i], end=', ')
+
