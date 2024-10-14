f5efafc574a7321fc07754d82665ba524b811e32
Bruno Souza <brunoaleixosh@gmail.com>
2024-10-14 16:44:27 -0300

    biblioteca.py

--- Atividades/POO Aulas/biblioteca.py
+++ Atividades/POO Aulas/biblioteca.py
@@ -49,3 +49,91 @@ class Pessoa:
                 print(f"{self.nome} não pode comer pois está falando.")
         else:
             print(f"{self.nome} não pode comer pois está dormindo.")
+
+class Animal:
+    def __init__(self,nome,cor):
+        self.nome = nome
+        self.cor = cor
+
+    def comer(self):
+        print(f"{self.nome} foi comer.")
+
+class Gato(Animal):
+    def __init__(self,nome,cor):
+        super().__init__(nome,cor)
+    def miar(self):
+        print(f"O {self.nome} faz MIAAAAAAAUUUUUUUUUUUUU")
+
+class Coelho(Animal):
+    def __init__(self,nome,cor):
+        super().__init__(nome,cor)
+    def pular(self):
+        print(f"O coelho {self.nome} pula...")
+
+class Vaca(Animal):
+    def __init__(self,nome,cor):
+        super().__init__(nome,cor)
+    def mugir(self):
+        print(f"A vaquinha {self.nome} começou a mugir, MUUUUUUUUUUUUUUUUUU.")
+
+class Cachorro(Animal):
+    def __init__(self,nome,cor):
+        super().__init__(nome,cor)
+    def latir(self):
+        print(f"O dog {self.nome} está latindo, Au Au.")
+
+class Atleta:
+    def __init__(self,nome,peso,idade):
+        self.nome=nome
+        self.peso=peso
+        self.idade=idade
+        self.aquecimento=False
+
+    def aposentar(self):
+        if self.idade >= 60:
+            print(f"O atleta {self.nome} está aposentado e não poderá competir.")
+        else:
+            print(f"O atleta {self.nome} está apto para competir.")
+
+    def aquecer(self):
+        print(f"O atleta {self.nome} está aquecido")
+        self.aquecimento=True
+
+class Corredor(Atleta):
+    def __init__(self,nome,peso,idade):
+        super().__init__(nome,peso,idade)
+    def correr(self):
+        if self.aquecimento==True:
+            if self.idade >= 60:
+                print(f"O atleta {self.nome} está aposentado e não poderá competir.")
+            else:
+                print(f"{self.nome} foi correr.")
+        else:
+            print(f"O atleta {self.nome} precisa aquecer.")
+
+class Nadador(Atleta):
+    def __init__(self,nome,peso,idade):
+        super().__init__(nome,peso,idade)
+    def nadar(self):
+        if self.aquecimento == True:
+            if self.idade >= 60:
+                print(f"O atleta {self.nome} está aposentado e não poderá competir.")
+            else:
+                print(f"{self.nome} foi nadar.")
+        else:
+            print(f"O atleta {self.nome} precisa aquecer.")
+
+class Ciclista(Atleta):
+    def __init__(self,nome,peso,idade):
+        super().__init__(nome,peso,idade)
+    def pedalar(self):
+        if self.aquecimento == True:
+            if self.idade >= 60:
+                print(f"O atleta {self.nome} está aposentado e não poderá competir.")
+            else:
+                print(f"{self.nome} foi pedalar.")
+        else:
+            print(f"O atleta {self.nome} precisa aquecer.")
+class TriAtleta(Corredor ,Nadador ,Ciclista):
+    def __init__(self,nome,peso,idade):
+        super().__init__(nome,peso,idade)
